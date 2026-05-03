46d45e0258efa98bc42410ba0c4946c440ddb3ee
Alexander Byakov <byakov-a55@mail.ru>
2026-05-03 22:45:55 +0300

    Add project files

---
+++ app/src/main/java/com/rimuru/android/sharedcart/ShoppingItem.kt
@@ -0,0 +1,8 @@
+package com.rimuru.android.sharedcart
+
+data class ShoppingItem(
+    val id: String = "",
+    val name: String = "",
+    val isChecked: Boolean = false,
+    val addedBy: String = ""
+)

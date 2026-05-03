c00e888544c88006bdfc16bf53d089cdc541409e
Alexander Byakov <byakov-a55@mail.ru>
2026-05-03 23:57:16 +0300

    changed datatype field lastUpdate

--- app/src/main/java/com/rimuru/android/sharedcart/domain/model/Product.kt
+++ app/src/main/java/com/rimuru/android/sharedcart/domain/model/Product.kt
@@ -1,9 +1,11 @@
 package com.rimuru.android.sharedcart.domain.model
 
+import java.time.Instant
+
 data class Product(
     val id: String,
     val listId: String,
     val name: String,
-    val lastUpdate: Long,
+    val lastUpdate: Instant,
     val isCompleted: Boolean
 )

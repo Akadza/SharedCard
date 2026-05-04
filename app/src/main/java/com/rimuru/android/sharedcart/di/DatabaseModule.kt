4e2d4a3acdce55634f7de2417ec19dea23277093
Alexander Byakov <byakov-a55@mail.ru>
2026-05-05 00:29:36 +0300

    implement hilt module for database injection

---
+++ app/src/main/java/com/rimuru/android/sharedcart/di/DatabaseModule.kt
@@ -0,0 +1,34 @@
+package com.rimuru.android.sharedcart.di
+
+import android.content.Context
+import androidx.room.Room
+import com.rimuru.android.sharedcart.data.local.SharedCartDatabase
+import com.rimuru.android.sharedcart.data.local.dao.ProductDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): SharedCartDatabase {
+        return Room.databaseBuilder(
+            context,
+            SharedCartDatabase::class.java,
+            "shared_cart_db"
+        ).build()
+    }
+
+    @Provides
+    fun provideProductDao(database: SharedCartDatabase): ProductDao {
+        return database.productDao()
+    }
+}
+

6a85805dfe5a675fadf3c5b36ba6f4e14899bb11
Alexander Byakov <byakov-a55@mail.ru>
2026-05-06 23:02:09 +0300

    add ShoppingListDao and ShoppingListEntity

--- app/src/main/java/com/rimuru/android/sharedcart/data/local/SharedCartDatabase.kt
+++ app/src/main/java/com/rimuru/android/sharedcart/data/local/SharedCartDatabase.kt
@@ -3,16 +3,19 @@ package com.rimuru.android.sharedcart.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.rimuru.android.sharedcart.data.local.converter.ProductConverter
+import com.rimuru.android.sharedcart.data.local.converter.DateConverter
 import com.rimuru.android.sharedcart.data.local.dao.ProductDao
+import com.rimuru.android.sharedcart.data.local.dao.ShoppingListDao
 import com.rimuru.android.sharedcart.data.local.entity.ProductEntity
+import com.rimuru.android.sharedcart.data.local.entity.ShoppingListEntity
 
 @Database(
-    entities = [ProductEntity::class],
+    entities = [ProductEntity::class, ShoppingListEntity::class],
     version = 1,
     exportSchema = false
 )
-@TypeConverters(ProductConverter::class)
+@TypeConverters(DateConverter::class)
 abstract class SharedCartDatabase : RoomDatabase() {
     abstract fun productDao(): ProductDao
+    abstract fun shoppingListDao(): ShoppingListDao
 }

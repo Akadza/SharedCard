6cd04b2bedf9c79587cad3d141ffad4891241185
Alexander Byakov <byakov-a55@mail.ru>
2026-05-06 23:02:56 +0300

    add new field for date -createdAt

--- app/src/main/java/com/rimuru/android/sharedcart/domain/model/ShoppingList.kt
+++ app/src/main/java/com/rimuru/android/sharedcart/domain/model/ShoppingList.kt
@@ -1,8 +1,11 @@
 package com.rimuru.android.sharedcart.domain.model
 
+import java.time.Instant
+
 data class ShoppingList (
     val id: String,
     val ownerId: String,
     val name: String,
+    val createdAt: Instant,
     val products: List<Product> = emptyList()
 )

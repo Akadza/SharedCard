86b39a25a614a0a847cb2bd1006c9cf87f7ad390
Alexander Byakov <byakov-a55@mail.ru>
2026-05-06 23:26:49 +0300

    implement ShoppingList with products relation

---
+++ app/src/main/java/com/rimuru/android/sharedcart/data/local/entity/CompositeShoppingList.kt
@@ -0,0 +1,13 @@
+package com.rimuru.android.sharedcart.data.local.entity
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class CompositeShoppingList (
+    @Embedded val shoppingList: ShoppingListEntity,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "listId"
+    )
+    val products: List<ProductEntity>
+)

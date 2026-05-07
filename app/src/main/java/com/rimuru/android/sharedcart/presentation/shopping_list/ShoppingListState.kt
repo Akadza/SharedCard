37a2026e9a855444341a9589553a8bb72650ad97
Alexander Byakov <byakov-a55@mail.ru>
2026-05-08 00:51:29 +0300

    add ShoppingListState for UI representation

---
+++ app/src/main/java/com/rimuru/android/sharedcart/presentation/shopping_list/ShoppingListState.kt
@@ -0,0 +1,9 @@
+package com.rimuru.android.sharedcart.presentation.shopping_list
+
+import com.rimuru.android.sharedcart.domain.model.ShoppingList
+
+data class ShoppingListState (
+    val lists: List<ShoppingList> = emptyList(),
+    val isLoading: Boolean = false,
+    val error: String? = null
+)

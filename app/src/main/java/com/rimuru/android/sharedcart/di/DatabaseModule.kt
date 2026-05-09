43de86f3d343d3fe2f54374f2e80e188dc67997c
Alexander Byakov <byakov-a55@mail.ru>
2026-05-10 01:28:10 +0300

    add new provider application scope

--- app/src/main/java/com/rimuru/android/sharedcart/di/DatabaseModule.kt
+++ app/src/main/java/com/rimuru/android/sharedcart/di/DatabaseModule.kt
@@ -2,6 +2,7 @@ package com.rimuru.android.sharedcart.di
 
 import android.content.Context
 import androidx.room.Room
+import com.google.api.ContextRule
 import com.rimuru.android.sharedcart.data.local.SharedCartDatabase
 import com.rimuru.android.sharedcart.data.local.dao.ProductDao
 import com.rimuru.android.sharedcart.data.local.dao.ShoppingListDao
@@ -10,6 +11,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
 
 
@@ -35,6 +39,12 @@ object DatabaseModule {
     @Provides
     fun provideProductDao(database: SharedCartDatabase): ProductDao {
         return database.productDao()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApplicationScope(): CoroutineScope {
+        return CoroutineScope(SupervisorJob() + Dispatchers.Default)
     }
 }
 

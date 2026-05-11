1688062c04fdc383041b96fe681b8605a37f5ed7
Alexander Byakov <byakov-a55@mail.ru>
2026-05-12 00:25:21 +0300

    add comment

--- app/src/main/java/com/rimuru/android/sharedcart/data/local/entity/CompositeShoppingList.kt
+++ app/src/main/java/com/rimuru/android/sharedcart/data/local/entity/CompositeShoppingList.kt
@@ -3,6 +3,7 @@ package com.rimuru.android.sharedcart.data.local.entity
 import androidx.room.Embedded
 import androidx.room.Relation
 
+// data transfer object (dto)
 data class CompositeShoppingList (
     @Embedded val shoppingList: ShoppingListEntity,
     @Relation(

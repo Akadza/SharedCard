2c35269cbdfccc54bfb3d4a8875d68f64cd329a1
Alexander Byakov <byakov-a55@mail.ru>
2026-05-15 00:17:41 +0300

    add main colors theme

--- app/src/main/java/com/rimuru/android/sharedcart/core/ui/theme/Color.kt
+++ app/src/main/java/com/rimuru/android/sharedcart/core/ui/theme/Color.kt
@@ -2,10 +2,26 @@ package com.rimuru.android.sharedcart.core.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val MainBlue = Color(0xFF4F8CFF)
+val SecondaryBlue = Color(0xFF7CB8FF)
+val BlueContainer = Color(0xFFEAF3FF)
+val BlueContainerDark = Color(0xFFDCEBFF)
+val LightBackground = Color(0xFFF7FAFF)
+val LightSurface = Color(0xFFFFFFFF)
+val TextPrimary = Color(0xFF1B2430)
+val TextSecondary = Color(0xFF6B7A90)
+val BorderLight = Color(0xFFD6E4FF)
+val SuccessBlue = Color(0xFF4DA3FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkBackground = Color(0xFF0F1724)
+val DarkSurface = Color(0xFF182233)
+val DarkPrimary = Color(0xFF8EC5FF)
+val DarkSecondary = Color(0xFF5FA8FF)
+val DarkContainer = Color(0xFF223048)
+val DarkTextPrimary = Color(0xFFF1F5F9)
+val DarkTextSecondary = Color(0xFF94A3B8)
+val DarkBorder = Color(0xFF314158)
+
+val White = Color.White
+val Black = Color.Black
+val ErrorRed = Color(0xFFFF6B6B)

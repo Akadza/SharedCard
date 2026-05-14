07e50622c6028523416683fcb2e0381e55f19516
Alexander Byakov <byakov-a55@mail.ru>
2026-05-15 00:16:56 +0300

    add main theme for light and dark

--- app/src/main/java/com/rimuru/android/sharedcart/core/ui/theme/Theme.kt
+++ app/src/main/java/com/rimuru/android/sharedcart/core/ui/theme/Theme.kt
@@ -8,35 +8,61 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = MainBlue,
+    secondary = SecondaryBlue,
+
+    background = LightBackground,
+    surface = LightSurface,
+    surfaceVariant = BlueContainer,
+
+    primaryContainer = BlueContainerDark,
+
+    onPrimary = White,
+    onSecondary = White,
+
+    onBackground = TextPrimary,
+    onSurface = TextPrimary,
+    onSurfaceVariant = TextSecondary,
+
+    outline = BorderLight,
+
+    error = ErrorRed
 )
+
+private val DarkColorScheme = darkColorScheme(
+
+    primary = DarkPrimary,
+    secondary = DarkSecondary,
+
+    background = DarkBackground,
+    surface = DarkSurface,
+    surfaceVariant = DarkContainer,
+
+    primaryContainer = DarkContainer,
+
+    onPrimary = DarkBackground,
+    onSecondary = White,
+
+    onBackground = DarkTextPrimary,
+    onSurface = DarkTextPrimary,
+    onSurfaceVariant = DarkTextSecondary,
+
+    outline = DarkBorder,
+
+    error = ErrorRed
+)
+
 
 @Composable
 fun SharedCartTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {

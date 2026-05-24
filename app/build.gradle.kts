061ee04ce14f9ba1e803db06f9698d66b98af82a
Alexander Byakov <byakov-a55@mail.ru>
2026-05-24 22:23:39 +0300

    add lifecycle runtime dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,8 @@ dependencies {
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.analytics)
 
+    implementation(libs.androidx.lifecycle.runtime.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)

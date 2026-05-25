45492af49d6b33204fabf32d9cc50b13034cda04
Alexander Byakov <byakov-a55@mail.ru>
2026-05-25 22:35:15 +0300

    add dimens

---
+++ app/src/main/java/com/rimuru/android/sharedcart/core/ui/theme/Dimens.kt
@@ -0,0 +1,29 @@
+package com.rimuru.android.sharedcart.core.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object SharedCartDimens {
+    val ScreenHorizontalPadding = 20.dp
+    val ScreenVerticalPadding = 16.dp
+
+    val CardRadius = 28.dp
+    val DialogRadius = 32.dp
+    val BottomSheetRadius = 32.dp
+    val TextFieldRadius = 20.dp
+    val ButtonRadius = 24.dp
+    val SmallChipRadius = 16.dp
+
+    val CardElevation = 2.dp
+    val PressedElevation = 4.dp
+
+    val IconContainerSize = 48.dp
+    val SmallIconContainerSize = 40.dp
+    val MainFabSize = 64.dp
+
+    val SpaceTiny = 4.dp
+    val SpaceSmall = 8.dp
+    val SpaceMedium = 12.dp
+    val SpaceLarge = 16.dp
+    val SpaceXLarge = 24.dp
+    val SpaceXXLarge = 32.dp
+}
